994b4e7aba24035561731a72cc5629d602f9a8e8
Kevin Matsuo <classicmatsuo@gmail.com>
2026-08-13 14:58:21 -0500

    retry deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "",
-  images: {
-    domains: ["https://fretboardsummit.org/"],
-    formats: ["image/webp"],
-    unoptimized: true,
-  },
+  // output: "export",
+  // basePath: "",
+  // images: {
+  //   domains: ["https://fretboardsummit.org/"],
+  //   formats: ["image/webp"],
+  //   unoptimized: true,
+  // },
 };
 
 module.exports = nextConfig;
